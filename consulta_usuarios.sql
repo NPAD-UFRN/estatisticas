1d51a6a690004067b75b196b9035222290a297ca
gabrielbarreto15 <gabriel.barreto.102@ufrn.edu.br>
2025-11-02 23:32:23 -0300

    remoção das colunas username e departamento usadas para teste de consultas

--- consulta_usuarios.sql
+++ consulta_usuarios.sql
@@ -1,12 +1,10 @@
 -- select usuarios coordenadores de pesquisa
-SELECT U.username, C.centro, D.departamento FROM centros AS C 
+SELECT C.centro FROM centros AS C 
 INNER JOIN departamentos AS D ON C.id = D.centroid
-INNER JOIN coordenadores_pesquisa AS CP ON CP.departamento_id = D.id
-INNER JOIN usuarios AS U ON U.id = CP.usuario_id;
+INNER JOIN coordenadores_pesquisa AS CP ON CP.departamento_id = D.id;
 
 -- select usuarios colaboradores
-SELECT U.username, C.centro, D.departamento FROM centros AS C 
+SELECT C.centro FROM centros AS C 
 INNER JOIN departamentos AS D ON C.id = D.centroid
 INNER JOIN coordenadores_pesquisa AS CP ON CP.departamento_id = D.id
-INNER JOIN colaboradores AS CO ON CO.usuario_cp_id = CP.usuario_id
-INNER JOIN usuarios AS U ON U.id = CP.usuario_id; 
+INNER JOIN colaboradores AS CO ON CO.usuario_cp_id = CP.usuario_id;
