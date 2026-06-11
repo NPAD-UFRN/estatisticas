0cf93d3052428cc30c5e157ac395bcd797700a2c
HaroldoWTSilva <haroldowatson@gmail.com>
2026-06-11 12:17:04 -0300

    sacct -a

--- dados_ocupacao_mensal.sh
+++ dados_ocupacao_mensal.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 #Script que coleta os dados necessários para o script ocupacao_mensal.py.
-#Ele deve calcular as datas de início e fim, e executar o seguinte comando:
-#sacct -X -P -S "$START_DATE" -E "$END_DATE" \
-#          --format=JobID,Partition,Submit,Start,end --noheader
 #Seja o mês de hoje Mh e seja o ano de hoje Ah, as datas são:
 #START_DATE = "(Ah-1)-Mh-01 00:00:00"
 #END_DATE = "Ah-Mh-01 00:00:00"
@@ -15,5 +12,5 @@ YEAR=$(date +%Y)
 START_DATE="$((YEAR-1))-${MONTH}-01 00:00:00"
 END_DATE="${YEAR}-${MONTH}-01 00:00:00"
 #Executar o comando sacct e salvar a saída em um arquivo
-sacct -X -P -S "$START_DATE" -E "$END_DATE" \
+sacct -a -X -P -S "$START_DATE" -E "$END_DATE" \
     --format=JobID,Partition,Submit,Start,end --noheader
